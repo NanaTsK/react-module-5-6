dad3915c0e9179e8debae4ad793af6c4dc0c12e9
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2023-10-16 13:41:00 +0200

    tasks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,12 @@
 import { Layout } from 'components/Layout/Layout';
-// import { AppBar } from 'components/AppBar/AppBar';
-// import { TaskForm } from 'components/TaskForm/TaskForm';
-// import { TaskList } from 'components/TaskList/TaskList';
+import { AppBar } from 'components/AppBar/AppBar';
+import { TaskForm } from 'components/TaskForm/TaskForm';
+import { TaskList } from 'components/TaskList/TaskList';
 
 export const App = () => {
   return (
     <Layout>
-      <button
+      {/* <button
         style={{
           display: 'flex',
           fontSize: 22,
@@ -14,11 +14,11 @@ export const App = () => {
         }}
       >
         BUTTON
-      </button>
+      </button> */}
 
-      {/* <AppBar />
+      <AppBar />
       <TaskForm />
-      <TaskList /> */}
+      <TaskList />
     </Layout>
   );
 };
